645a5ca1bedaec1f76039d45f80f06a864fb2e2d
Edwin_Mutembei <mutembeiedwin87@gmail.com>
2024-04-17 15:50:29 +0300

    server logs analysis

---
+++ log_files.py
@@ -0,0 +1,38 @@
+
+
+import re
+
+ #Creates an empty list wherein you can append error logs.
+error = [] 
+
+ #Creates an empty list wherein you can add warning logs.
+warning = [] 
+ 
+ #Opens and reads the source file in this case the given txt file or the log directory.
+with open('log1.txt', "r") as f:
+#Reads all the lines.
+    log_data = f.readlines()
+ 
+    for line in log_data: 
+        #Searches the line with the words "error" or “warning" and “fail”. Uses capturing group for conditionals.
+        result = re.search(r"\w*level=(warning|error)*[\w ]*[\=\"\w \.]*[fF]ailed", line)
+        if result != None:
+           #If this particular capture group has the word "error"
+           if result[1] == 'error':  
+              #Add the line into the error list
+              error.append(line)    
+           #If this particular capture group has the word "warning"
+     
+           if result[1] == 'warning':
+           #Add the line into the warning list. 
+              warning.append(line)   
+
+
+ #Creates a new file with all the listed lines that have errors.
+with open('error_message.txt', 'w', newline='') as error_list:
+    error_list.writelines("%s\n" % x for x in error)  
+
+
+#Creates a new file with all the listed lines that have failure warnings.
+with open('warning_message.txt', 'w+', newline='') as warning_list:
+    warning_list.writelines("%s\n" % y for y in warning)
---
+++ practice.py
@@ -0,0 +1,14 @@
+name = '$$edwin mute mbei ehfh ddj fjdj jacjdjk cajdqwjd  hedbejh bje jqjdcuj dj cbqwj  ueuqiq  dhuwgu ueuqgygqeuhqi'
+# print(name.split())
+
+name1 = ('edwin mutembei')
+for x in name1:
+    part = x.split()
+    i = False
+    print(part)
+
+    if i in part:
+        i_index = part.index(i)
+        print(i_index)
+
+
